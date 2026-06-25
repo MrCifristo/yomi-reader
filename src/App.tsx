fe8ee9f7c7b8121fc767874bf4d8026190f302fa
Milton Beltrán <miltonbeltran@ufm.edu>
2026-06-24 21:47:56 -0600

    chore: scaffold yomi-reader vite+react+vitest

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="app">
+      <header className="topbar">
+        <span className="brand">YOMI.READER</span>
+      </header>
+    </div>
+  );
+}
